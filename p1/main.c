8a067df25c8f873b106c5d377b227864943d137e
2023125082 <tirtmvls2@kau.kr>
2026-04-20 15:32:52 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int answer, guess, count = 0;
+
+    scanf("%d", &answer);
+
+    do {
+        scanf("%d", &guess);
+        count += 1;
+
+        if (guess > answer)
+            printf("%d>?\n", guess);
+        else if (guess < answer)
+            printf("%d<?\n", guess);
+        else
+            printf("%d==?\n", guess);
+
+    } while (guess != answer);
+
+    printf("%d\n", count);
+
+    return 0;
+}
